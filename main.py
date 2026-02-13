85741c7a0914bdba2b23f83758e5afb0c82903c0
Enri258 <equilessanchez@gmail.com>
2026-02-13 20:25:04 +0100

    second commit

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"""
         Screen width: {constants.SCREEN_WIDTH}
@@ -17,7 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
